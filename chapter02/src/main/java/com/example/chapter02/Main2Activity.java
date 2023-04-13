f16ab09fec803fb359f948423d107ad1496ff83f
john.huang <john.huang@cloudwise.com>
2023-04-13 11:08:40 +0800

    增加一个反向跳转页

--- chapter02/src/main/java/com/example/chapter02/Main2Activity.java
+++ chapter02/src/main/java/com/example/chapter02/Main2Activity.java
@@ -2,7 +2,10 @@ package com.example.chapter02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //改造部分: 增加跳转
+        new Handler(Looper.myLooper()).postDelayed(() -> {
+            startActivity(new Intent(Main2Activity.this,Main3Activity.class));
+        },3000);
+
     }
 }
